6754b4f055cad3252cf06f887c2b7291cf7973e1
maxim5255555554548nn <117581646+maxim5255555554548nn@users.noreply.github.com>
2022-11-06 17:22:30 +0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    void Start()
+    {
+        print("hello, world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+ 
+}
